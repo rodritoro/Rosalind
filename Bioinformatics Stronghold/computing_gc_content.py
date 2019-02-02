73d56aca4c7fac5fdc4d2c473b17cd5e4e853744
Rodrigo Toro <r0dritj@hotmail.com>
2019-02-02 19:36:17 +0000

    Complete computing_gc_content

---
+++ Bioinformatics Stronghold/computing_gc_content.py
@@ -0,0 +1,102 @@
+"""
+Problem
+The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. Note that the reverse complement of any DNA string has the same GC-content.
+
+DNA strings must be labeled when they are consolidated into a database. A commonly used method of string labeling is called FASTA format. In this format, the string is introduced by a line that begins with '>', followed by some labeling information. Subsequent lines contain the string itself; the first line to begin with '>' indicates the label of the next string.
+
+In Rosalind's implementation, a string in FASTA format will be labeled by the ID "Rosalind_xxxx", where "xxxx" denotes a four-digit code between 0000 and 9999.
+
+Given: At most 10 DNA strings in FASTA format (of length at most 1 kbp each).
+
+Return: The ID of the string having the highest GC-content, followed by the GC-content of that string. Rosalind allows for a default error of 0.001 in all decimal answers unless otherwise stated; please see the note on absolute error below.
+
+Sample Dataset
+>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT
+Sample Output
+Rosalind_0808
+60.919540
+
+Note on Absolute Error
+We say that a number x is within an absolute error of y to a correct solution if x is within y of the correct solution. For example, if an exact solution is 6.157892, then for x to be within an absolute error of 0.001, we must have that |x−6.157892|<0.001, or 6.156892<x<6.158892.
+
+Error bounding is a vital practical tool because of the inherent round-off error in representing decimals in a computer, where only a finite number of decimal places are allotted to any number. After being compounded over a number of operations, this round-off error can become evident. As a result, rather than testing whether two numbers are equal with x=z, you may wish to simply verify that |x−z| is very small.
+
+The mathematical field of numerical analysis is devoted to rigorously studying the nature of computational approximation.
+"""
+
+import argparse
+
+sample_dataset = """>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT"""
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-file', required=False)
+    args = parser.parse_args()
+    return args
+
+def parse_fasta(fasta, is_file=True):
+    if is_file == True:
+        with open(fasta) as f:
+            fasta_lines = f.read().split('\n')
+    else:
+        fasta_lines = fasta.split('\n')
+
+    strings = {}
+    for i in range(len(fasta_lines)):
+        if fasta_lines[i].startswith('>'):
+            new_string_id = fasta_lines[i].replace('>','')
+            new_string = ''
+            next = i + 1
+            while next < len(fasta_lines) and not fasta_lines[next].startswith('>'):
+                new_string += fasta_lines[next]
+                next += 1
+            strings[new_string_id] = new_string
+
+    return strings
+
+def get_gc_content(string):
+    total_gc = string.count('G') + string.count('C')
+    gc_content = (float(total_gc ) / len(string)) * 100
+    return gc_content
+
+def get_highest_gc_content(strings):
+    highest_gc = 0
+    highest_gc_string_id = str
+
+    for id in strings:
+        string = strings[id]
+        gc_content = get_gc_content(string)
+        if gc_content > highest_gc:
+            highest_gc = gc_content
+            highest_gc_string_id = id
+
+    return (highest_gc_string_id, highest_gc)
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    if args.input_file:
+        strings = parse_fasta(args.input_file)
+    else:
+        strings = parse_fasta(sample_dataset, is_file=False)
+
+    result = get_highest_gc_content(strings)
+    print(result[0])
+    print(result[1])
