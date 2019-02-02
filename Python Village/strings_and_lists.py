4e10dffd35918d59f21f0c6f415351ad67cca77f
Rodrigo Toro <r0dritj@hotmail.com>
2019-02-02 11:34:50 +0000

    Complete strings and lists

--- Python Village/strings_and_lists.py
+++ Python Village/strings_and_lists.py
@@ -1,13 +1,38 @@
-sample_string = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.''
+import argparse
+
+sample_string = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.'
 sample_list = [22, 27, 97, 102]
 
+real_string = 'ItfPgxhzHBoKWgzyH5XXBX0Tf7j3fxBbqlLnkoTMfCbZQoqxweDAi1jZsyqO1c0pMSffjavRR4pZCdz4AlE6NBMiRMegophrysbeC6DVmYuEZeAwSM65ywip4Blnz0himalayensisT6OXsc53d8ORdNfT5jrycUaOlpNoJkN2e3RwwbVCgcHIeX.'
+real_list = [89, 97, 126, 137]
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file')
+    args = parser.parse_args()
+    return args
+
+def get_string_and_list(file):
+    with open(file) as f:
+        input = f.read()
+        input = input.split()
+    string = input[0]
+    list = input[1:]
+    for i in range(len(list)):
+        list[i] = int(list[i])
+    return string, list
+
 def strings_and_lists(string, list):
-    answer = ''
-
-
-
-    return answer
+    start1 = list[0]
+    end1 = list[1] + 1
+    start2 = list[2]
+    end2 = list[3] + 1
+    cut1 = string[start1:end1]
+    cut2 = string[start2:end2]
+    return ' '.join([cut1, cut2])
 
 
 if __name__ == '__main__':
-    strings_and_lists(sample_string, sample_list)
+    args = get_args()
+    string, list = get_string_and_list(args.input_file)
+    print(strings_and_lists(string, list))
