31f17719667104c76f380731cbb1a71cc4bad708
Rodrigo Toro <r0dritj@hotmail.com>
2019-02-02 11:14:30 +0000

    Add directories; add strings and lists exercise

---
+++ Python Village/strings_and_lists.py
@@ -0,0 +1,13 @@
+sample_string = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain.''
+sample_list = [22, 27, 97, 102]
+
+def strings_and_lists(string, list):
+    answer = ''
+
+
+
+    return answer
+
+
+if __name__ == '__main__':
+    strings_and_lists(sample_string, sample_list)
