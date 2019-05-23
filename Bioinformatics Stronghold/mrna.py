a5e5f59147d85bf98b3254fd478185e2a5b6f940
rtorojimen <rtorojimenez@illumina.com>
2019-05-23 16:21:08 +0100

    Create function to get load codon table

--- Bioinformatics Stronghold/mrna.py
+++ Bioinformatics Stronghold/mrna.py
@@ -1,17 +1,43 @@
 """
+Problem
+For positive integers a and n, a modulo n (written amodn in shorthand) is the remainder when a is divided by n. For example, 29mod11=7 because 29=11×2+7.
 
+Modular arithmetic is the study of addition, subtraction, multiplication, and division with respect to the modulo operation. We say that a and b are congruent modulo n if amodn=bmodn; in this case, we use the notation a≡bmodn.
+
+Two useful facts in modular arithmetic are that if a≡bmodn and c≡dmodn, then a+c≡b+dmodn and a×c≡b×dmodn. To check your understanding of these rules, you may wish to verify these relationships for a=29, b=73, c=10, d=32, and n=11.
+
+As you will see in this exercise, some Rosalind problems will ask for a (very large) integer solution modulo a smaller number to avoid the computational pitfalls that arise with storing such large numbers.
+
+Given: A protein string of length at most 1000 aa.
+
+Return: The total number of different RNA strings from which the protein could have been translated, modulo 1,000,000. (Don't neglect the importance of the stop codon in protein translation.)
+
+Sample Dataset
+MA
+Sample Output
+12
 """
 
 import argparse
 import time
 
-sample_dataset = ''
+sample_dataset = 'MA'
+
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', required=False)
     args = parser.parse_args()
     return args
+
+
+def get_codon_table():
+    with open('codon_table.json') as f:
+        codon_table = json.loads(f.read())
+    return codon_table
+
+
+
 
 
 if __name__ == '__main__':
