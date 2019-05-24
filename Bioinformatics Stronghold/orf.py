ab8f78526cb4ac46728965beaef136ff6fed8b4c
rtorojimen <rtorojimenez@illumina.com>
2019-05-24 13:42:08 +0100

    Set up orf

---
+++ Bioinformatics Stronghold/orf.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+import argparse
+import time
+
+sample_dataset = ''
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-file', required=False)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    START = time.time()
+    args = get_args()
+
+    if args.input_file:
+        pass
+
+    else:
+        pass
+
+    print('\nFinished in {} seconds\n'.format(time.time() - START))
