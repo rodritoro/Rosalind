7fb28bce1e7fa29a8abc3499ac78e5e361bd1bdc
rtorojimen <rtorojimenez@illumina.com>
2019-05-28 11:42:17 +0100

    Create parse_fasta and get_reverse_strand functions

--- Bioinformatics Stronghold/orf.py
+++ Bioinformatics Stronghold/orf.py
@@ -1,8 +1,12 @@
 """
 Problem
-Either strand of a DNA double helix can serve as the coding strand for RNA transcription. Hence, a given DNA string implies six total reading frames, or ways in which the same region of DNA can be translated into amino acids: three reading frames result from reading the string itself, whereas three more result from reading its reverse complement.
+Either strand of a DNA double helix can serve as the coding strand for RNA transcription. Hence, a given DNA string
+implies six total reading frames, or ways in which the same region of DNA can be translated into amino acids: three
+reading frames result from reading the string itself, whereas three more result from reading its reverse complement.
 
-An open reading frame (ORF) is one which starts from the start codon and ends by stop codon, without any other stop codons in between. Thus, a candidate protein string is derived by translating an open reading frame into amino acids until a stop codon is reached.
+An open reading frame (ORF) is one which starts from the start codon and ends by stop codon, without any other stop
+codons in between. Thus, a candidate protein string is derived by translating an open reading frame into amino acids
+until a stop codon is reached.
 
 Given: A DNA string s of length at most 1 kbp in FASTA format.
 
@@ -22,13 +26,46 @@ MTPRLGLESLLE
 import argparse
 import time
 
-sample_dataset = ''
+sample_dataset = """>Rosalind_99
+AGCCATGTAGCTAACTCAGGTTACATGGGGATGACCCCGCGACTTGGATTAGAGTCTCTTTTGGAATAAGCCTGAATGATCCGAGTAGCATCTCAG
+"""
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', required=False)
     args = parser.parse_args()
     return args
+
+
+def parse_fasta(fasta, is_file=True):
+    if is_file == True:
+        with open(fasta) as f:
+            fasta_lines = f.read().split('\n')
+    else:
+        fasta_lines = fasta.split('\n')
+
+    strings = {}
+    for i in range(len(fasta_lines)):
+        if fasta_lines[i].startswith('>'):
+            new_string_id = fasta_lines[i].replace('>','')
+            new_string = ''
+            next = i + 1
+            while next < len(fasta_lines) and not fasta_lines[next].startswith('>'):
+                new_string += fasta_lines[next]
+                next += 1
+            strings[new_string_id] = new_string
+
+    return strings
+
+
+def get_reverse_strand(forward_strand):
+    inverse_forward = forward_strand[::-1]
+    reverse_strand = ''
+    complements = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    for base in inverse_forward:
+        reverse_strand += complements[base]
+
+    return reverse_strand
 
 
 if __name__ == '__main__':
@@ -39,6 +76,14 @@ if __name__ == '__main__':
         pass
 
     else:
-        pass
+        strings = parse_fasta(sample_dataset, is_file=False)
+        print(strings)
+
+    for string_id in strings:
+        string = strings[string_id]
+        complement = get_reverse_strand(string)
+        print(string)
+        print(complement)
+
 
     print('\nFinished in {} seconds\n'.format(time.time() - START))
